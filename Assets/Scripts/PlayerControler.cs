a366b20a977d663320427f929e5765b10524429f
Laurits Lauritsen <laurits.mumberg@gmail.com>
2016-09-07 19:35:17 +0200

    roll ball
    skrald

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+	private Rigidbody rb;
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (x, y, z);
+
+		rb.AddForce (Vector3);
+	}
+} 
